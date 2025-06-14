49748c7a86ac85a82ee35ac432b9fdea6f96e567
arjun <arjunakrishna71@gmail.com>
2025-06-15 01:17:40 +0530

    4th Commit: updatig the Notes by adding Slice,fill,filter,splice

--- program01.js
+++ program01.js
@@ -132,3 +132,61 @@
 // the 3 -> Represents the Not Replacing end index value.
 // The Inclusice is including this value also.
 // The Exclusive is Excluding this value and replacing the values before this value and Excluding this value. 
+// let array = new Array(8);
+// console.log(typeof array);
+// let res = array.fill('V',7,9);
+// console.log(typeof res);  //Output: Object
+// // output:
+// // [ <8 empty items> ]
+// // [ <7 empty items>, 'V' ]
+
+// ----------------------------------FILTER----------------------------------
+// --> Filter will return an Array based on the condition.
+// Need to have a CALLBACK function to return the function.
+// We need to STORE the value of the returned array in a new variable to use it in future.
+// let arr = new Array(88,99,45,100);
+// let res = arr.filter(SampleFunction);
+// console.log(res);
+// function SampleFunction(e){
+//     return e>88;
+// }
+// --> Filter is mostly used in the time where we need to get a new array based on the condition.
+// let obj = {
+//     name:"Jagan",
+//     age:24,
+//     IsMarried:false
+// };
+// let data = obj.keys.filter((e)=>e.name);
+// console.log(data);
+// --> If you try this using an Object this will not work
+
+// &^&%&^&*&()*()^&^&*^@#(@*&(*#)())------------------Slice and Splice------------------&^&^^%*(&(*()*)*)(_)
+// --------------SLICE------------------------
+// Syntax: array.slice(Inclusive Starting index,Exclusive End Index)
+// --> Here, the Inclusive means it will includes in the value and the Exclusive means it will return before this index value.
+// --> SLICE is like SLICING a CAKE in two slices to get the peice.
+// let array = [546,754,856,789,888,987,908];
+// let res = array.slice(1,4);
+// // here the 1 is the inclusive value on INDEX '1' and the 4 is the exclusive value of INDEX '4'
+// // console.log(res);
+// // So the result will be: on index1 inclusive 754 and on the index4 888 exclusing. OUTPUT : [754,856,789]
+// let res2 = array.slice(4);
+// console.log(res2);
+// // --> If you give only a single value in slice then it will slice away the values which are below the index value. Here the result will [888,987,908]
+// // -->The single value represent the Inclusive value and it is not having the EXCLUSIVE value.
+// ---------------SPLICE------------------------
+// Syntax: array.splice(Inclusive Starting indexedDB,Total number of values to be deleted COUNT,The Value to be REPLACED)
+// --> It is same as the FILL method but here we are passing the replacable values as the LAST parameter and the we are sending the second parameter as Count of nmbers to be deleted
+// ----->FILL is mostly used for INITIALIZATION purpose while the STATE needs to be in the inital state while LOADING but the SPLICE can be USED in the CORE Functionalities.
+// let array = [546,754,856,789,888,987,908];
+// let res = array.splice(1,3,"Hello","After","Splice");
+// console.log(res);
+// console.log(array);
+// -->In SPLICE you can directly use the main array to get the updated array
+// let array = [1,23,55];
+// let res= array.splice(2);
+// console.log(res);
+// console.log(array);
+
+
+
